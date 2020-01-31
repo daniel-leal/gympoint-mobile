204828d5de3311b31db7bce49367bd928b16387f
daniel-leal <danielleal94@gmail.com>
2020-01-30 22:33:56 -0300

    init repo

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import SignIn from '~/pages/SignIn';
+
+import Checkin from '~/pages/Checkin';
+
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Checkin,
+          },
+          {
+            resetOnBlur: true,
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#EE4E62',
+              inactiveTintColor: '#999999',
+              style: {
+                backgroundColor: '#fff',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      }
+    )
+  );
